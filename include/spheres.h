22be2d4a0d959c3d8de85fcd8cec62c9d0ffd839
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-04-05 21:00:47 -0300

    feat: add ray-sphere intersections

---
+++ include/spheres.h
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   spheres.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 18:05:41 by mdias-ma          #+#    #+#             */
+/*   Updated: 2023/04/05 19:53:12 by mdias-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPHERES_H
+# define SPHERES_H
+
+# include "matrices.h"
+# include "tuples.h"
+
+typedef t_tuple	t_point;
+typedef t_tuple	t_vector;
+
+typedef struct s_ray
+{
+	t_point		origin;
+	t_vector	direction;
+}	t_ray;
+
+typedef struct s_sphere
+{
+	t_point		origin;
+	float		radius;
+	t_matrix	transform;
+}	t_sphere;
+
+typedef union u_object
+{
+	t_sphere	sphere;
+}	t_object;
+
+typedef struct s_intersection
+{
+	float					t;
+	t_object				object;
+	struct s_intersection	*next;
+}	t_intersection;
+
+/* rays.c */
+t_ray			new_ray(t_point origin, t_vector direction);
+t_point			position(t_ray ray, float distance);
+t_ray			transform(t_ray ray, t_matrix matrix);
+
+/* spheres.c */
+t_intersection	*intersect(t_sphere sphere, t_ray ray);
+void			set_transform(t_sphere *sphere, t_matrix transform);
+
+/* intersections.c */
+t_intersection	*intersection(float t, t_sphere sphere);
+void			insert_intersection(t_intersection **xs, t_intersection *i);
+t_intersection	*hit(t_intersection *xs);
+void			intersections_free(t_intersection **xs);
+
+#endif
---
+++ src/rays/rays.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rays.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 18:16:46 by mdias-ma          #+#    #+#             */
+/*   Updated: 2023/04/05 20:50:23 by mdias-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "spheres.h"
+
+t_ray	new_ray(t_point origin, t_vector direction)
+{
+	return ((t_ray){
+		origin, direction
+	});
+}
+
+t_point	position(t_ray ray, float distance)
+{
+	return (add(ray.origin, multiply(ray.direction, distance)));
+}
+
+t_ray	transform(t_ray r, t_matrix m)
+{
+	return (
+		new_ray(
+			multiply_tuple_matrix(m, r.origin),
+			multiply_tuple_matrix(m, r.direction))
+	);
+}
