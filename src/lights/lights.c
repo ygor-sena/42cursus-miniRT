6f0f67774927a6658a21c75af2b5def6920ffe99
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-04-14 10:51:40 -0300

    refactor(lights): check return from malloc

--- src/lights/lights.c
+++ src/lights/lights.c
@@ -6,17 +6,17 @@
 /*   By: yde-goes <yde-goes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 11:11:56 by yde-goes          #+#    #+#             */
-/*   Updated: 2023/04/13 17:00:02 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/04/13 20:29:13 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lights.h"
 
-t_light	*point_light(t_tuple	point, t_color intensity)
+t_light	*point_light(t_tuple point, t_color intensity)
 {
 	t_light	*light;
 
-	light = malloc(sizeof(t_light));
+	light = oom(malloc(sizeof(t_light)));
 	light->position = point;
 	light->intensity = intensity;
 	return (light);
