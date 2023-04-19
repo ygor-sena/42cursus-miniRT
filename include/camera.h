844544810b0e6a18694d73a720fbb3d2681eb049
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-04-19 19:33:12 -0300

    feat: implement a camera

--- include/camera.h
+++ include/camera.h
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 12:29:11 by mdias-ma          #+#    #+#             */
-/*   Updated: 2023/04/18 16:20:06 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/04/19 16:48:18 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,21 @@
 
 # include "tuples.h"
 # include "matrices.h"
+# include "rays.h"
 
 typedef struct s_camera
 {
-	int			hsize;
-	int			vsize;
+	float		hsize;
+	float		vsize;
 	float		field_of_view;
+	float		half_width;
+	float		half_height;
+	float		pixel_size;
 	t_matrix	transform;
 }	t_camera;
 
 t_matrix	view_transform(t_tuple from, t_tuple to, t_tuple up);
-t_camera	new_camera(int hsize, int vsize, float field_of_view);
+t_camera	new_camera(float hsize, float vsize, float field_of_view);
+t_ray		ray_for_pixel(t_camera *camera, float px, float py);
 
 #endif
