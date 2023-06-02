9ca8a22047d3e8d659f69e05401792dcb2807060
ygor_sena <yde-goes@student.42sp.org.br>
2023-06-02 16:56:57 -0300

    feat(pattern): implement UV mapping for checkered pattern

--- src/patterns/patterns.c
+++ src/patterns/patterns.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   patterns.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: yde-goes <yde-goes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 15:22:30 by yde-goes          #+#    #+#             */
-/*   Updated: 2023/05/26 16:29:32 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/06/02 16:06:26 by yde-goes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shapes.h"
+#include "patterns.h"
 
 t_pattern	new_pattern(t_color a, t_color b)
 {
@@ -19,6 +20,8 @@ t_pattern	new_pattern(t_color a, t_color b)
 		.pattern_at = stripe_at,
 		.a = new_color(a.red, a.green, a.blue),
 		.b = new_color(b.red, b.green, b.blue),
+		.texture_map.uv_pattern = uv_checkers(0, 0, a, b),
+		.texture_map.uv_map = planar_map,
 		.transform = get_identity_matrix(),
 		.inverse = get_identity_matrix(),
 		.transpose = get_identity_matrix()
@@ -32,12 +35,21 @@ t_color	stripe_at(t_pattern pattern, t_point world_point)
 	return (pattern.b);
 }
 
+//if ((int)(floor(world_point.x) + floor(world_point.y)
+//	+ floor(world_point.z)) % 2 == 0)
+//	return (pattern.a);
+//return (pattern.b);
 t_color	checkered_at(t_pattern pattern, t_point world_point)
 {
-	if ((int)(floor(world_point.x) + floor(world_point.y)
-		+ floor(world_point.z)) % 2 == 0)
-		return (pattern.a);
-	return (pattern.b);
+	t_checker	uv;
+
+	uv = pattern.texture_map.uv_map(world_point);
+	return (
+		uv_pattern_at(
+			pattern.texture_map.uv_pattern,
+			uv.width,
+			uv.height
+		));
 }
 
 t_color	pattern_at_shape(t_pattern pattern, t_shape *shape, t_point world_point)
